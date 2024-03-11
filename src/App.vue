2126dae6e171db27dde2796e0ae148a2d4853439
rafaelabarbieric <barbieridacruzrafaela@gmail.com>
2024-03-11 16:56:59 -0300

    criação de lista

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+  import { computed, ref } from 'vue'
+
+  let newCity = ref('')
+  let listCitys = ref(['São Paulo','Rio de Janeiro','Belo Horizonte','Salvador','Fortaleza','Curitiba','Manaus','Recife','Porto Alegre','Brasília'])
+
+  let organize = computed (() =>{
+    return listCitys.value.sort()
+  }
+  )
+  
+  function add() {
+    listCitys.value.push(newCity.value)
+    newCity.value = ''
+  }
+
+  function deleteCity(item) {
+    listCitys.value.splice(item, 1)
+  }
+  
+</script>
+<template>
+  <input type="text" v-model="newCity">
+  <button @click="add">Adicionar</button>
+  <ul>
+    <li v-for="item in organize">{{ item }} <button @click="deleteCity(item)">X</button></li>
+  </ul>
+</template>
